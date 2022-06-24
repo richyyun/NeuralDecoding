45fbc1fc3f47837a8f4d0f7ba4f1237119601752
Richy Yun <richyjyun@gmail.com>
2022-06-24 16:10:31 -0700

    First NN implementation

--- SiameseNet.py
+++ SiameseNet.py
@@ -11,9 +11,11 @@ import random
 import numpy as np
 import pandas as pd
 import os
-import torch
 import time
+import torch
 import torchvision
+from torch import nn
+
 
 
 # Define device
@@ -70,17 +72,117 @@ class Triplet(torch.utils.data.Dataset):
         anchor = self.loadData(self.alabel[idx])
         positive = self.loadData(self.plabel[idx])
         negative = self.loadData(self.nlabel[idx])
-        return anchor, positive, negative, # self.atype[idx], self.ntype[idx] # For debugging
+        return anchor, positive, negative, idx, # self.atype[idx], self.ntype[idx] # For debugging
     
     # Load data, all saved data should be parquet files
     def loadData(self, filepath):
         data = pd.read_parquet(filepath)
-        return pd.DataFrame.to_numpy(data)
+        data = pd.DataFrame.to_numpy(data)
+        data = torch.from_numpy(data)
+        data = data.to(self.device)
+        return data
     
 
 # Set up path to dataset
 exppath = os.getcwd() + '/Data/Spanky-170901-131421'
 data = Triplet(exppath, device)
+
+# Split dataset into train and test data
+testsize = int(0.2*np.floor(len(data)))
+trainsize = len(data) - testsize
+train_data, test_data = torch.utils.data.random_split(data, [trainsize, testsize])
+
+# Define dataloaders
+batch_size = 32     
+trainloader = torch.utils.data.DataLoader(train_data, batch_size=batch_size, shuffle=True)
+testloader = torch.utils.data.DataLoader(test_data, batch_size=batch_size, shuffle=False)
+
+
+''' Define model '''
+class siamese(nn.Module):                    
+    def __init__(self, bidirectional): 
+        super(siamese,self).__init__()    
+        
+        # Convolutional layers
+        kernel_size = 3         
+        padding = int((kernel_size-1)/2)   # Keep same length with stride 1
+        self.Conv1 = nn.Conv1d(in_channels=96, out_channels=32, kernel_size=kernel_size, padding=padding)
+        
+        lin = 1500  # input length is set to 1500
+        lout = 150  # desired output length. Too long and it will slow down the LSTM
+        kernel_size = lin-lout+1
+        self.MaxPool1 =  nn.MaxPool1d(kernel_size)
+        
+        kernel_size = 3         
+        padding = int((kernel_size-1)/2)   # Keep same length with stride 1
+        self.Conv2 = nn.Conv1d(in_channels=32, out_channels=8, kernel_size=kernel_size, padding=padding)
+        
+        lin = lout
+        seq_len = 15
+        kernel_size = lin-seq_len+1
+        self.MNaxPool2 = nn.MaxPool2d(kernel_size)        
+        
+        # LSTM
+        self.hidden_size = 10           # Number of hidden features
+        self.num_layers = 1             # Number of stacks
+        self.D = 1                      # Constant for bidirectional LSTMs
+        if bidirectional:
+            self.D = 2
+            
+        # LSTM requires input size (number of features), the sequence length, and whether it's bidirectional
+        self.LSTM = nn.LSTM(8, self.hidden_size, self.num_layers, bidirectional=bidirectional, batch_first=True)
+
+        # Rather than 1 output, have as many as the sequence length (many-to-many)
+        self.Linear = nn.Linear(self.D * self.hidden_size * seq_len, seq_len) 
+        
+        
+    # Have to reinitialize h0 and c0 each pass as the shape depends on the batch size
+    def h0c0(self, batch_size):
+        h0 = torch.zeros(self.D * self.num_layers, batch_size, self.hidden_size).to(self.device)
+        c0 = torch.zeros(self.D * self.num_layers, batch_size, self.hidden_size).to(self.device)
+        self.hidden = (h0, c0)
+
+    # Forward propagation
+    def forward(self, x):      
+        
+        # Go through convolutional layers
+        conv = self.Conv1(x)
+        conv = self.MaxPool1(conv)
+        conv = self.Conv2(conv)
+        conv = self.MaxPool2(conv)
+        
+        # Initialize hidden and cell states
+        batch_size = x.shape[0] 
+        self.h0c0(batch_size)
+        
+        # LSTM layer
+        lstm, self.hidden = self.LSTM(x, self.hidden)
+        
+        # Fully connected layer
+        fc = torch.flatten(lstm, start_dim=1)
+        y = self.Linear(fc)
+        
+        return y, #lstm, conv # For debugging
+
+
+def distance(a, b):
+    return np.sum(np.square(a-b), axis=-1)
+
+
+# Initialize model
+model = siamese(False)
+model = model.float()
+model = model.to(device)
+
+
+
+
+
+
+
+
+
+
 
 
 
